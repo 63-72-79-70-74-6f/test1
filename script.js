0c7d207de62d6c0d424bd6178656120616df5a8e
DeXx <sralibaba1234567890@gmail.com>
2025-07-20 13:33:14 +0200

    update

---
+++ script.js
@@ -0,0 +1,136 @@
+console.log("JS TEST");
+document.getElementById('p').innerHTML = 'Hello World';
+/*
+let a = 4; //num
+console.log(typeof(a))
+
+let b = "JS"; //string
+console.log(typeof(b))
+
+let c = 'JS' //string
+console.log(typeof(c))
+
+let d = false; //boolean
+console.log(typeof(d))
+
+let e = null; //null
+console.log(typeof(e))
+
+let f
+console.log(typeof(f))
+*/
+
+
+
+// let a = 15
+// let b = 3
+
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+// console.log(++a)
+// console.log(--b)
+
+//console.log(2==2)
+
+
+
+// let x = 2
+
+// console.log(x+=2)
+// console.log(x-=2)
+// console.log(x*=2)
+// console.log(x/=2)
+
+
+//let x = 1
+// let x = -1
+
+// console.log('Przesunięcie w lewo: '+ (x<<1));
+// console.log('Przesunięcie w prawo: '+ (x>>1));
+
+// console.log('Przesunięcie w prawo z wypełnieniem: '+ (x>>>1));
+
+
+
+// let x = 115;
+
+// if (x==15){
+//     console.log('Wartość x = 15')
+// }else if (x<15){
+//     console.log('Wartość x jest mniejsza od 15')
+// }else if /*(x>15)*/{
+//     console.log('Wartość x jest większa od 15')
+// }
+
+
+
+// let x = 115
+// if( x % 2 === 0){
+//     console.log('Jest to liczba parzysta')
+// }else{
+//     console.log('Jest to liczba nieparzysta')
+// }
+
+
+
+// let x = 116;
+
+// const czyParzysta = (x % === 0)? 'parzysta' : 'nieparzysta';
+
+// console.log(czyParzysta);
+
+
+
+// let x = 1
+// switch (x){
+//     case 1:
+//     case 2:
+//     case 3:
+//         if (x==1) console.log('x wynosi 1')
+//         else if (x==2)  console.log('x wynosi 2')
+//         else console.log('x wynosi 3')
+//         break;
+//     case 4:
+//         console.log('x wynosi 4')
+//         break;
+//     case 5:
+//         console.log('x wynosi 5')
+//         break;
+//     default:
+//         console.log('żadna z powyższych' )
+// }
+
+
+
+let x = 3
+let y = 5
+let z = 0
+
+// if (x>y){
+//     if (x>z){
+//         console.log('Największa liczba to '+x)
+//     }else{
+//         console.log('Najwieksza wartość to '+z)
+//     }else if (y>x){
+//         if (y>z){
+//         console.log('Największa liczba to '+x)
+//     }else{
+//         console.log('Najwieksza wartość to '+z)
+//     }
+//     }
+//}
+
+if (x!=y && x!=z && y!=z){
+    if(x>y && x>z){
+        console.log('Największa wartość to '+x)
+    }
+    if(y>x && y>z){
+        console.log('Największa wartość to '+y)
+    }
+    if(z>x && z>y){
+        console.log('Największa wartość to '+z)
+    }
+}
